f21c9b62d78f2508069953e6bc512e3ac0ce2a56
João Victor <97927354+jvictorpdl@users.noreply.github.com>
2023-04-28 00:08:05 -0300

    Prova 2

---
+++ P2 Computação Gráfica/Piramide/piramideQuad.js
@@ -0,0 +1,34 @@
+import * as THREE from 'three';
+
+      const scene = new THREE.Scene();
+      const width = window.innerWidth;
+      const height = window.innerHeight;
+      const camera =  new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+
+      const renderer = new THREE.WebGLRenderer();
+      renderer.setClearColor(0xFFFFFF); // COLANDO COR NO FUNDO
+      renderer.setSize(window.innerWidth, window.innerHeight);
+      document.body.appendChild(renderer.domElement);
+
+      const texture = new THREE.TextureLoader().load( '../textures/texturaPiramide.jpg' );
+    
+      const geometry = new THREE.ConeGeometry(4, 3, 4);
+      const material = new THREE.MeshBasicMaterial({
+        map: texture,
+      });
+
+      const piramideQuadrada = new THREE.Mesh(geometry, material);
+      scene.add(piramideQuadrada);
+
+      camera.position.z = 10;
+
+      const animate = function () {
+        requestAnimationFrame(animate);
+
+        // cone.rotation.x += 0.01;
+        piramideQuadrada.rotation.y += 0.01;
+
+        renderer.render(scene, camera);
+      };
+
+      animate();
---
+++ P2 Computação Gráfica/Prisma/prismaWire.js
@@ -0,0 +1,29 @@
+import * as THREE from 'three';
+
+    const scene = new THREE.Scene();
+    const width = window.innerWidth;
+    const height = window.innerHeight;
+    const camera =  new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+
+    const renderer = new THREE.WebGLRenderer();
+    renderer.setClearColor(0xFFFFFF); // COLANDO COR NO FUNDO
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    document.body.appendChild(renderer.domElement);
+
+    const geometry = new THREE.CylinderGeometry( 1, 1, 3, 3 ); 
+    const material = new THREE.MeshBasicMaterial( { color: 0x0000ff, wireframe: true } );
+    const cylinder = new THREE.Mesh( geometry, material ); 
+
+    scene.add( cylinder );
+    camera.position.z = 5;
+
+      const animate = function () {
+        requestAnimationFrame( animate );
+
+        cylinder.rotation.x += 0.01;
+        cylinder.rotation.y += 0.01;
+
+        renderer.render(scene, camera);
+      };
+
+      animate();
